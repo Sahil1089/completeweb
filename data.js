0a884ba8d03282334c336c769da2772ed018f934
Sahil1089 <soniskkr1234@gmail.com>
2024-11-26 00:54:07 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,58 @@
+let destination=[{
+    id:"001",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjZuBEb0S9UeyqHCWcRSeBkm5e2-m1h-OCeg&s",
+    name:"BODH GAYA",
+    loc:"GAYA,BIHAR"
+
+},{
+    id:"002",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTExXFp4rCDM6Ekkuukzi481T8I_QPo7mzevA&s",
+    name:"rajgir",
+    loc:"GAYA,BIHAR"
+},
+{
+    id:"003",
+    image:"https://images.bhaskarassets.com/web2images/521/2021/06/29/orig_29kaimur-07_1624985200.jpg",
+    name:"kaimur hills",
+    loc:"kaimur,BIHAR"
+},
+{
+    id:"004",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfBcO84BNbv-QDJiBnH9MpCAZz40wuf79fJA&s",
+    name:"sher shah suri tomb",
+    loc:"sasaram,BIHAR"
+},
+{
+    id:"005",
+    image:"https://media2.thrillophilia.com/images/photos/000/374/398/original/1625654404_12._TUTLA_BHAWANI_WATERFALL__ROHTAS.jpg?w=753&h=450&dpr=1.5",
+    name:"tutla bhawani waterfall",
+    loc:"tilauthu,BIHAR"
+}
+
+
+]
+
+let footerdata=[
+    {
+        icon:"fa-solid fa-envelope",
+        text:"tourofbihar@gmail.com"
+    },
+    {
+        icon:"fa-brands fa-instagram",
+        text:"tour_of _bihar"
+    },
+    {
+        icon:"fa-brands fa-facebook",
+        text:"bihartourism"
+
+    },
+    {
+        icon:"fa-brands fa-youtube",
+        text:"bihar tourism"
+    }
+]
+
+let tradition =[
+   
+    
+]
